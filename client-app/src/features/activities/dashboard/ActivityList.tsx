7468ed74f19327b3715db504d5088e742d154f57
erfansydkzm <seyedkazemierfan@yahoo.com>
2022-04-01 11:03:21 +0430

    add uuid

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -5,6 +5,7 @@ import { Activity } from "../../../app/models/activity";
 interface Props {
     activities: Activity[];
     selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
 }
 
 export default function ActivityList({ activities, selectActivity }: Props) {
