a3cef12d444825883fa324b5ed62c7049269e7d9
erfansydkzm <seyedkazemierfan@yahoo.com>
2022-04-15 00:41:16 +0430

    mobx

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Header, List } from 'semantic-ui-react'
+import { Button, Container, Header, List } from 'semantic-ui-react'
 import { Activity } from '../models/activity';
 import NavBar from './navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -7,6 +7,7 @@ import { v4 as uuid } from 'uuid';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite'
 
 
 function App() {
@@ -84,6 +85,7 @@ function App() {
       <NavBar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
         <h2>{activityStore.title}</h2>
+        <Button content='add' positive onClick={activityStore.setTitle} />
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
@@ -101,4 +103,4 @@ function App() {
   );
 }
 
-export default App;
+export default observer(App);
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,11 +1,16 @@
-import { makeObservable, observable } from 'mobx';
+import { action, makeObservable, observable } from 'mobx';
 
 
 export default class ActivityStore {
     title = "Hello Word";
     constructor() {
         makeObservable(this, {
-            title: observable
+            title: observable,
+            setTitle: action
         })
+    }
+
+    setTitle = () => {
+        this.title = 'new content';
     }
 }
